93723921424bffa57e021add0f86a6c1daf77915
Gjermund Skobba <gjermund@skobba.net>
2019-07-01 20:48:15 +0200

    Display IP on epaper

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # esp8266-epaper-wifi
 
-# Libs
+# Libraries
+https://docs.platformio.org/en/latest/librarymanager/quickstart.html
+https://www.arduinolibraries.info/libraries/arduino-json
+
 GxEPD
+ArduinoJSON
+## Search for libs
+https://platformio.org/lib
+
+# Wifi and json
+https://circuits4you.com/2019/01/11/nodemcu-esp8266-arduino-json-parsing-example/
 
 # Platformio
 brew install platformio
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,14 @@
 #include <Fonts/FreeMonoBold12pt7b.h>
 #include <GxIO/GxIO_SPI/GxIO_SPI.h>
 #include <GxIO/GxIO.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
 #define ONE_WIRE_BUS D4
+
+const char* wifiName = "ssid";
+const char* wifiPass = "pass";
 
 float tempC = 0;
 
@@ -12,14 +18,38 @@ GxIO_Class io(SPI, 15, 4, 5);
 GxEPD_Class display(io,  5,  16);
 
 void setup() {
+  // Setup serial
   Serial.begin(9600);
   Serial.println();
+
+  // Setup wifi
+  delay(10);
+  Serial.println();
+  
+  Serial.print("Connecting to ");
+  Serial.println(wifiName);
+ 
+  WiFi.begin(wifiName, wifiPass);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());   //You can get IP address assigned to ESP
+
+
+  // Setup display
   display.init();
+
 }
 
 
 
-void show()
+void show(String msg)
 {
   const char* name = "FreeMonoBold12pt7b";
   const GFXfont* f = &FreeMonoBold12pt7b;
@@ -31,16 +61,17 @@ void show()
   display.setRotation(2);
   display.setFont(f);
   display.setTextColor(GxEPD_WHITE);
-  //display.fillRect(box_x, box_y, box_w, box_h, GxEPD_BLACK);
-  display.fillRect(0, 0, 380, 280, GxEPD_BLACK);
+  display.fillRect(box_x, box_y, box_w, box_h, GxEPD_BLACK);
+  //display.fillRect(0, 0, 380, 280, GxEPD_BLACK);
 
-  display.setCursor(box_x+15, cursor_y+88);
-  display.print("dette er en test");
+  display.setCursor(box_x+15, cursor_y+138);
+  display.print(msg);
   display.updateWindow(box_x, box_y, box_w, box_h, true);
+  //display.updateWindow(box_x, 200, box_w, 200, true);
 }
 
 void loop() {
-  show();
+  show(WiFi.localIP().toString());
   display.update();
   delay(5000);
 }
